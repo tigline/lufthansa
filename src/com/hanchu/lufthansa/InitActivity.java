2c3bd4027fb42ca13b287f8e7563ec87334e7dc4
tigline <projectgame@163.com>
2015-07-01 18:01:57 +0800

    construct

---
+++ src/com/hanchu/lufthansa/InitActivity.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package com.hanchu.lufthansa;
+
+//import cn.eoe.leigo.splash.GuideActivity;
+//import cn.eoe.leigo.splash.MainActivity;
+import com.hanchu.lufthansa.R;
+import com.hanchu.lufthansa.InitActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Window;
+
+/**
+ * @Project Lufthansa	
+ * @author houxb
+ * @Date 2015-7-1
+ */
+public class InitActivity extends Activity {
+	
+	boolean isFirstIn = false;
+
+	private static final int GO_HOME = 1000;
+	private static final int GO_GUIDE = 1001;
+	// 延迟3秒
+	private static final long SPLASH_DELAY_MILLIS = 3000;
+
+	private static final String SHAREDPREFERENCES_NAME = "first_pref";
+
+	/**
+	 * Handler:跳转到不同界面
+	 */
+	private Handler mHandler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case GO_HOME:
+				goHome();
+				break;
+			case GO_GUIDE:
+				//goGuide();
+				break;
+			}
+			super.handleMessage(msg);
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_init);
+
+		init();
+	}
+
+	private void init() {
+		// 读取SharedPreferences中需要的数据
+		// 使用SharedPreferences来记录程序的使用次数
+		SharedPreferences preferences = getSharedPreferences(
+				SHAREDPREFERENCES_NAME, MODE_PRIVATE);
+
+		// 取得相应的值，如果没有该值，说明还未写入，用true作为默认值
+		isFirstIn = preferences.getBoolean("isFirstIn", true);
+
+		// 判断程序与第几次运行，如果是第一次运行则跳转到引导界面，否则跳转到主界面
+		if (!isFirstIn) {
+			// 使用Handler的postDelayed方法，3秒后执行跳转到MainActivity
+			mHandler.sendEmptyMessageDelayed(GO_HOME, SPLASH_DELAY_MILLIS);
+		} else {
+			mHandler.sendEmptyMessageDelayed(GO_GUIDE, SPLASH_DELAY_MILLIS);
+		}
+
+	}
+
+	private void goHome() {
+		Intent intent = new Intent(InitActivity.this, MainActivity.class);
+		InitActivity.this.startActivity(intent);
+		InitActivity.this.finish();
+	}
+	/*
+	private void goGuide() {
+		Intent intent = new Intent(InitActivity.this, GuideActivity.class);
+		InitActivity.this.startActivity(intent);
+		InitActivity.this.finish();
+	}
+	*/
+}
--- src/com/hanchu/lufthansa/MainActivity.java
+++ src/com/hanchu/lufthansa/MainActivity.java
@@ -4,12 +4,37 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	private Button startButtom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        
+        startButtom = (Button) findViewById(R.id.bt_start);
+        
+        startButtom.setOnClickListener(this);
     }
+	/* (non-Javadoc)
+	 * @see android.view.View.OnClickListener#onClick(android.view.View)
+	 */
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.bt_start:
+			
+			break;
+
+		default:
+			break;
+		}
+	}
 }
